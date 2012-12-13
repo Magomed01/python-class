237a8cf877a38e888ef8f47be6eb50074591ed9d
Robert Theis <robert.m.theis@gmail.com>
2012-12-12 19:39:40 -0800

    add new code from Teague

--- unemployment.py
+++ unemployment.py
@@ -1,78 +1,72 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 28 18:34:40 2012
+#/usr/bin/env python
 
-@author: robert
-"""
-
+import os
 import csv
+import sys
+
 from BeautifulSoup import BeautifulSoup
 
-txt = """
-Teague, Smith, Novato, 415-555-1234
-John, Smith, San Rafael, 415-555-5678
+UNKNOWN_COLOR = (128, 128, 128)  # Some type of Grey
 
-"""
+COLOR_TEMPLATE_OLD = "#%0.2X%0.2X%0.2X"     # Hex color using " % " style
+COLOR_TEMPLATE_NEW = "#{:02X}{:02X}{:02X}"  # Hex color using .format()
 
-#lines = txt.split('\n')
-#people = []
-#for line in lines:
-#  tokens = line.split(',')
-#  people.append({
-#    'first': tokens[0],
-#    'last': tokens[1],
-#    'city': tokens[2],
-#    'phone': tokens[3],    
-#  })
-  
-# Split each line by commas after getting lines by splitting on newline
-#[Person(*l.split(',')) for l in txt.split('\n')]
-# Equiv.: [Person(*['Teague', 'Smith', 'Novato', '415-555-1234']) ... ]
-# which resolves to: [Person('Teague', 'Smith', 'Novato', ...) ... ]
+# We can split lines up like this if we really want
+STYLE_TEMPLATE = "font-size:12px;fill-rule:nonzero;stroke:#000000;"\
+                 "stroke-opacity:1;stroke-width:0.1;stroke-miterlimit:4;"\
+                 "stroke-dasharray:none;stroke-linecap:butt;"\
+                 "marker-start:none;stroke-linejoin:bevel;fill:{fill}"
 
-# Open a CSV file and parse it
-levels = {}
-with open('unemployment09.csv', 'r') as f:
-  reader = csv.reader(f, delimiter=',')
-  for row in reader:
-    try:    
-      id = ''.join(row[1:3])
-      rate = float(row[-1].strip()) # get the last column as a float
-      levels[id] = rate
-    except:
-      print "error!"
-    #print rate
-  
-  lowest = min(levels.values())
-  highest = max(levels.values())
-  spread = highest - lowest
-  deltas = { k: ((v - lowest)/spread)*255 for k,v in levels.items() }
+source_file = sys.argv[1] if len(sys.argv) > 1 else "unemployment09.csv"
 
-  
-# Open an SVG file and color-code counties with unemployment rates
+if len(sys.argv) > 2:
+    output_file = sys.argv[2]
+else:
+    folder = os.path.dirname(source_file)
+    source_name = os.path.basename(source_file)
+    base, ext = os.path.splitext(source_name)
+    output_file = os.path.join(folder, "{0}.svg".format(base))
+
+rates = {}
+with open(source_file, 'r') as data:
+    reader = csv.reader(data, delimiter=",")
+    for row in reader:
+        try:
+            county_id = row[1].strip() + row[2].strip()
+            rate = float(row[-1].strip())
+            rates[county_id] = rate
+        except:
+            print "Error in {}".format(row)
+
+lowest = min(rates.values())
+highest = max(rates.values())
+spread = highest - lowest
+# Let's create a little helper function for ourselves
+
+# Make the delta for each county a value between 0 and 255 with
+# the highest unemployment being 0 and the lowest being 255
+# We use this to create a color that is "less white" and
+# "more red" based on unemployment rates.
+calc_delta = lambda rate: 255 - int(round(255 * (rate - lowest) / spread))
+deltas = {k: calc_delta(v) for (k, v) in rates.items()}
+
 with open('counties.svg', 'r') as svg:
-    # Treat weird 'sodipodi' tags as self-closing
-    soup = BeautifulSoup(svg.read(), selfClosingTags=['defs', 'sodipodi: namedview'])
-counties = soup.findAll('path')
-
-# We'll replace the style tags in the SVG text
-# but it would be beter to do a dictionary of key/value pairs here...
-STYLE = "font-size:12px;fill-rule:nonzero;stroke:#000000;stroke-opacity:1;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:"
+    soup = BeautifulSoup(svg.read(),
+                         selfClosingTags=['defs', 'sodipodi:namedview'])
+paths = soup.findAll('path')
+counties = (p for p in paths if p['id'] not in ["State_Lines", "separator"])
 
 for county in counties:
-  county_id = county['id']
-  if county_id in ('separator', 'State_lines'):
-    continue
-  if county_id in deltas:
-    delta = deltas[county_id]
-    sub = int(255 - delta) # we're putting together a hex color like #FF0000
-    color = ("%0.2x"*3).format(255, sub, sub) # repeat hex value three times
-    county['style'] = STYLE + color
-    print color
+    county_id = county['id']
+    if county_id in rates:
+        delta = deltas[county_id]
+        color = (255, delta, delta)
+    else:
+        color = UNKNOWN_COLOR
 
-with open('new-counties-unemp.svg', 'w') as f:
-  f.write(soup.prettify)
+    color_code = COLOR_TEMPLATE_OLD % color  # Using old style "%" operator
+    color_code = COLOR_TEMPLATE_NEW.format(*color)  # Using .format()
+    county['style'] = STYLE_TEMPLATE.format(fill=color_code)
 
-
-
-
+with open(output_file, 'w') as svg:
+    svg.write(soup.prettify())
